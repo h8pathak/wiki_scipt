75ef65225ee4cdfb4d3fd8175abc3458e6b9ba0f
h8pathak <harshit_pathak@yahoo.com>
2016-11-02 15:01:47 +0530

    Update wiki_final.py

--- wiki_final.py
+++ wiki_final.py
@@ -7,18 +7,63 @@ def find_sentence(wiki_text):
 
     print ("Enter the citation no:")
     n = input()
+    lis = []
+
+    while 1:
     
-    result = re.search(r'([^].!?\[]+[.!?]?)\[' + n + ']', wiki_text.strip()).group(1)
-    print (result)
+        match = re.search(r'([^].!?\[]+[.!?]?)\[' + n + ']', wiki_text.strip())
+        if match:
+            result = match.group(1)
+        else:
+            break
+
+        if result not in lis:
+            lis.append(result)
+
+        pos = wiki_text.find(result)
+        new_start = pos + len(result)
+
+        wiki_text = wiki_text[new_start:]
+        
+    print ("Result:")
+    for l in lis:
+        print (l)
 
 def find_citation(wiki_text):
 
     print ("Enter the sentence to find its citation number:")
     sentence = input()
 
-    n = wiki_text[wiki_text.find(sentence)+len(sentence)+1]
-    print(n)
-  
+    pos = wiki_text.find(sentence)+len(sentence)+1
+    lis = []
+    
+    while 1 :
+        
+        result = ""
+        result += wiki_text[pos]
+        while 1:
+              pos += 1
+              try:
+                  temp = int(wiki_text[pos])
+                  result += str(temp)
+              except:
+                  break
+                        
+        lis.append(result)
+
+        pos += 2
+        
+        try:
+            temp = int(wiki_text[pos])
+        except:
+            break
+
+    print ("Result:")
+    for l in lis:
+        print (l)
+
+        
+
 
 if __name__ ==  "__main__":
 
@@ -39,14 +84,20 @@ if __name__ ==  "__main__":
     for link in soup.findAll('p'):
         wiki_text += link.text
 
-    print ("What do you want to do?")
-    print("""Enter \n1. Get the sentence with a citation number\n2. Get the citation number of a sentence:""")
+    c='y'
+    while c=='y':
 
-    option = int(input())
+        print ("What do you want to do?")
+        print("""Enter \n1. Get the sentence with a citation number\n2. Get the citation number of a sentence:""")
 
-    if option == 1:
-        find_sentence(wiki_text)
-    elif option == 2:
-        find_citation(wiki_text)
-    else:
-        print ("Invalid Entry")
+        option = int(input())
+
+        if option == 1:
+            find_sentence(wiki_text)
+        elif option == 2:
+            find_citation(wiki_text)
+        else:
+            print ("Invalid Entry")
+
+        print ("Wanna enter more (y/n):")
+        c = input()
